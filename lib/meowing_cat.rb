faaddc4a64ecc8c9ae428ef96b2173cfde817c17
Nick Bazaldua <bazalnick@gmail.com>
2020-08-04 20:35:41 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,4 +1,17 @@
 ## code your solution here. 
 class Cat
   
-  def 
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  attr_accessor :name
+  
+  def meow
+    puts "meow!"
+  end
+end
